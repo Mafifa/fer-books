9c8cffe1672757edfd8c4f415521c919cb60434f
Mafifa <120286152+Mafifa@users.noreply.github.com>
2024-03-24 13:01:34 -0400

    Condig Eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+    env: {
+        "browser": true,
+        "es2021": true
+    },
+    extends: ["plugin:astro/recommended", "standard-with-typescript"],
+    overrides: [
+        {
+            env: {
+                "node": true
+            },
+            files: [
+                ".eslintrc.{js,cjs}", "*.astro"
+            ],
+            parser: "astro-eslint-parser",
+            parserOptions: {
+                parser: "@typescript-eslint/parser",
+                extraFileExtensions: [".astro"],
+                sourceType: "script"
+            }
+        }
+    ],
+    parserOptions: {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    rules: {
+    }
+}
