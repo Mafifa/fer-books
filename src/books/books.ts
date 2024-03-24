4330a6cf069a944ccc998933cefb01cf353a3874
Mafifa <120286152+Mafifa@users.noreply.github.com>
2024-03-24 18:00:21 -0400

    Info about all the books in the website

---
+++ src/books/books.ts
@@ -0,0 +1,16 @@
+interface Book {
+  id: string,
+  title: string,
+  img: string,
+  autor?: string,
+  opinion?: string
+}
+
+export const books: Book[] = [
+  { id: "Camino-de-los-reyes", title: "El Camino de los Reyes", img: "https://cdn.agapea.com/B-de-Bolsillo-Ediciones-B-/El-camino-de-los-reyes-El-Archivo-de-las-Tormentas-1--i6n21535506.jpg" },
+  { id: "Camino-de-los-reyes", title: "El Camino de los Reyes", img: "https://cdn.agapea.com/B-de-Bolsillo-Ediciones-B-/El-camino-de-los-reyes-El-Archivo-de-las-Tormentas-1--i6n21535506.jpg" },
+  { id: "Camino-de-los-reyes", title: "El Camino de los Reyes", img: "https://cdn.agapea.com/B-de-Bolsillo-Ediciones-B-/El-camino-de-los-reyes-El-Archivo-de-las-Tormentas-1--i6n21535506.jpg" },
+  { id: "Camino-de-los-reyes", title: "El Camino de los Reyes", img: "https://cdn.agapea.com/B-de-Bolsillo-Ediciones-B-/El-camino-de-los-reyes-El-Archivo-de-las-Tormentas-1--i6n21535506.jpg" },
+  { id: "Camino-de-los-reyes", title: "El Camino de los Reyes", img: "https://cdn.agapea.com/B-de-Bolsillo-Ediciones-B-/El-camino-de-los-reyes-El-Archivo-de-las-Tormentas-1--i6n21535506.jpg" },
+
+];
