e2a76f1a104458620e5679f6d8638842e20ea05c
Mafifa <120286152+Mafifa@users.noreply.github.com>
2024-03-25 18:26:05 -0400

    Fix error on body try 1

--- src/utils/navigation.js
+++ src/utils/navigation.js
@@ -3,10 +3,8 @@ function checkIsNavigationSupported () {
 }
 
 async function fetchPage (url) {
-  // Cargamos la página de destino usando fetch para obtener el HTML
   const response = await fetch(url);
   const text = await response.text();
-  // Extraemos el contenido de la etiqueta HTML dentro de la etiqueta body usando regex
   const [, data] = text.match(/<body[^>]*>([\s\S]*)<\/body>/i) ?? ['', ''];
   return data;
 }
@@ -18,19 +16,15 @@ export function startViewTransition () {
 
   window.navigation.addEventListener('navigate', async (event) => {
     const toURL = new URL(event.destination.url);
-    // ¿Es una página externa?
     if (location.origin !== toURL.origin) {
       return;
     }
 
-    // Si no es una página externa, interceptamos la navegación y cargamos la nueva página
     event.intercept({
       async handler () {
         const data = await fetchPage(toURL.href);
 
-        // Utilizamos la API de View Transition
         document.startViewTransition(() => {
-          // Actualizamos la vista con el nuevo contenido
           document.body.innerHTML = data;
           document.documentElement.scrollTop = 0; // Volvemos al principio de la página
         });
