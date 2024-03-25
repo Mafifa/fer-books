dcd15b07fac39b3cec15ba8fc83cc0fb4447c5dc
Mafifa <120286152+Mafifa@users.noreply.github.com>
2024-03-25 18:13:33 -0400

    Change the eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,7 @@ module.exports = {
         "browser": true,
         "es2021": true
     },
-    extends: ["plugin:astro/recommended", "standard-with-typescript"],
+    extends: ["plugin:astro/recommended"],
     overrides: [
         {
             env: {
